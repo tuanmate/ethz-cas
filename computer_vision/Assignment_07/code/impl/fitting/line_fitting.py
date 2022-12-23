a1f44392fb53bf002d687612099c9d0adb54804e
tuan <tuan.mate@gmail.com>
2022-12-23 17:16:00 +0100

    Add RANSAC

--- computer_vision/Assignment_07/code/impl/fitting/line_fitting.py
+++ computer_vision/Assignment_07/code/impl/fitting/line_fitting.py
@@ -9,6 +9,8 @@ def least_square(x,y):
 	# TODO
 	# return the least-squares solution
 	# you can use np.linalg.lstsq
+	A = np.vstack([x, np.ones(len(x))]).T
+	k, b = np.linalg.lstsq(A, y, rcond=None)[0]
 	return k, b
 
 def num_inlier(x,y,k,b,n_samples,thres_dist):
@@ -16,7 +18,8 @@ def num_inlier(x,y,k,b,n_samples,thres_dist):
 	# compute the number of inliers and a mask that denotes the indices of inliers
 	num = 0
 	mask = np.zeros(x.shape, dtype=bool)
-
+	mask = np.abs(b + k * x - y) / np.sqrt(1+np.square(k)) < thres_dist
+	num = np.count_nonzero(mask)
 	return num, mask
 
 def ransac(x,y,iter,n_samples,thres_dist,num_subset):
@@ -26,6 +29,18 @@ def ransac(x,y,iter,n_samples,thres_dist,num_subset):
 	b_ransac = None
 	inlier_mask = None
 	best_inliers = 0
+	for _ in range(iter):
+		indices = list(range(n_samples))
+		subset_indices = random.sample(indices, num_subset)
+		subset_indices = np.asarray(subset_indices)
+		subset_x = x[subset_indices]
+		subset_y = y[subset_indices]
+		k, b = least_square(subset_x, subset_y)
+		num, mask = num_inlier(x, y, k, b, n_samples, thres_dist)
+		if num > best_inliers:
+			k_ransac = k
+			b_ransac = b
+			inlier_mask = mask
 
 	return k_ransac, b_ransac, inlier_mask
 
